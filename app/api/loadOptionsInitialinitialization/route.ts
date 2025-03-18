72d467781efa5bf6b719c6e7f345229dbe3cc63a
toan7042 <toan.hoangjoy2@gmail.com>
2025-03-18 16:45:42 +0700

    tf

--- app/api/loadOptionsInitialinitialization/route.ts
+++ app/api/loadOptionsInitialinitialization/route.ts
@@ -7,16 +7,15 @@ export async function GET() {
     const basePath = path.join(process.cwd(), "app/data/optionapiinitialinitialization");
 
     // LOAD .TXT, MỖI DÒNG LÀ 1 PHẦN TỬ
-    const countryData = fs.readFileSync(path.join(basePath, "file_option_country.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
+    const countryData = ["NONE", ...fs.readFileSync(path.join(basePath, "file_option_country.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean)];
     const nameGenData = fs.readFileSync(path.join(basePath, "file_option_namegen.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
     const phoneGenData = fs.readFileSync(path.join(basePath, "file_option_phonegen.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
     const mailGenData = fs.readFileSync(path.join(basePath, "file_option_mailgen.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
     const languageData = fs.readFileSync(path.join(basePath, "file_option_language.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
     const userAgentData = fs.readFileSync(path.join(basePath, "file_option_useragent.txt"), "utf-8").split("\n").map(line => line.trim()).filter(Boolean);
-
     // LOAD JSON
-    const phoneModels = ["RAND-ITEM", ...JSON.parse(fs.readFileSync(path.join(basePath, "jsonPhoneInfo_data.json"), "utf-8")).devices.map((device: { MODEL: string }) => device.MODEL)];
-    const carrierData = ["AS-COUNTRY", ...JSON.parse(fs.readFileSync(path.join(basePath, "jsonCarrier_data.json"), "utf-8")).map((device: { ISO: string }) => device.ISO)];    
+    const phoneModels = ["NONE", "RAND-ITEM", ...JSON.parse(fs.readFileSync(path.join(basePath, "jsonPhoneInfo_data.json"), "utf-8")).devices.map((device: { MODEL: string }) => device.MODEL)];
+    const carrierData = ["NONE", "AS-COUNTRY", ...JSON.parse(fs.readFileSync(path.join(basePath, "jsonCarrier_data.json"), "utf-8")).map((device: { ISO: string }) => device.ISO)];    
 
     return NextResponse.json({
       staperchangetypePHONEINFO: phoneModels,
